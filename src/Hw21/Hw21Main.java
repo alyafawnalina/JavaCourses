e275721d0238d76df395a8bd5329d3b758deb066
Alina <kobiakovaalina@gmail.com>
2024-05-27 20:03:32 +0300

    commited hw21

---
+++ src/Hw21/Hw21Main.java
@@ -0,0 +1,14 @@
+package Hw21;
+
+public class Hw21Main {
+
+    public static void main(String[] args) {
+
+        Pair<Integer, String> pair = new Pair<>(23, "Hello world");
+
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+
+    }
+
+}
